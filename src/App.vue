770a8ea1e28cea416abca890ed046e0ea9a76e61
Diego Reyes <diegorele13@gmail.com>
2026-08-17 17:51:50 -0500

    feat(app): componente raíz

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+onMounted(() => userStore.hydrate())
+</script>
+
+<template>
+  <RouterView />
+</template>
